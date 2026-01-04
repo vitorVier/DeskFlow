8f0457317cfe99ad4b770a9b562f163eb693e4a9
vitorVier <vierrvitor@gmail.com>
2026-01-04 15:35:16 -0300

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container";
+
+export default function Dashboard() {
+    return(
+        <Container>
+            <h1>Dashboard Page</h1>
+        </Container>
+    )
+}
